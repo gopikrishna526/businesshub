3595bb5e60a8a641e33f9ddc49368128675d7954
Bhukya GopiKrishna <bhukyagopi66@gmail.com>
2026-09-08 09:09:06 +0530

    Initial project commit

---
+++ src/main/java/com/businesshub/BusinesshubApplication.java
@@ -0,0 +1,13 @@
+package com.businesshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinesshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinesshubApplication.class, args);
+	}
+
+}
